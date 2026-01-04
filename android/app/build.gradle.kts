4d5f3658fd7f0c704a7c25970b8b77384a5be4cb
ChungMin Wen <chungmin.wen@gmail.com>
2026-01-04 23:36:00 +1100

    fix(build): use build-tools 35.0.0 for compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     namespace = "com.cmwen.prompt_loop"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-    buildToolsVersion = "36.1.0"
+    buildToolsVersion = "35.0.0"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
